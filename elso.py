32433a84f34de0089bc7bd86acd0a92bbaeef12d
mpatrikik <m.patrik01@gmail.com>
2023-09-14 12:30:59 +0200

    hi

--- elso.py
+++ elso.py
@@ -1,2 +1,12 @@
-print("Salama a leykum!")
-print("Salsa Y picante")
+"""print('Salama \na \nleykum!')
+print('Salsa Y picante')
+print('Szia', 'lajos!', sep='-')
+print('Mizu báttya?')"""
+
+print('Ez'
+      'most'
+      'több sorba'
+      'kerül')
+
+nev="Pali"
+print('Szia',nev)
